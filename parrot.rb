466b81f6c7418d238541e43b0b916fc18a23df79
Akim Clerge <akimclerge@gmail.com>
2019-10-14 19:01:55 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot( a = "Pretty bird!")
- if a ==! "Squawk!"
- puts "Squawk!"
- "Squawk!"
- end
-
+def parrot(phrase = "Pretty bird!")
+  puts "Squawk!"
+  "Squawk!"
+end
 
  
   
